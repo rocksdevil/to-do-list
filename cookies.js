54e521dcde8510709cefc3560bf1bf993c297517
pawel-galkowski <p.galkowski@ik.pl>
2020-08-17 09:18:02 +0200

    Update cookies.js

--- cookies.js
+++ cookies.js
@@ -1,78 +1,42 @@
-// function setCookie(key, value, expiry) {
-//     let values = Object.assign({}, value);
-//     var expires = new Date();
-//     expires.setTime(expires.getTime() + (expiry * 24 * 60 * 60 * 1000));
-//     document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();
-// } 
-
-function setCookie(name, value) {
-    var cookie = [name, '=', JSON.stringify(value), '; domain=.', window.location.host.toString(), '; path=/;'].join('');
+const cookies = {
+  setCookie: function (name, value) {
+    var cookie = [
+      name,
+      "=",
+      JSON.stringify(value),
+      "; domain=.",
+      window.location.host.toString(),
+      "; path=/;",
+    ].join("");
     document.cookie = cookie;
-}
+  },
 
-function getCookie(name) {
-    var result = document.cookie.match(new RegExp(name + '=([^;]+)'));
+  getCookie: function (name) {
+    var result = document.cookie.match(new RegExp(name + "=([^;]+)"));
     result && (result = JSON.parse(result[1]));
     return result;
+  },
+
+  saveTodos: function(tasks) {
+    window.localStorage.setItem("tasks", JSON.stringify(tasks));
+    this.showTodos();
+},
+
+  loadTodos: function () {
+    return JSON.parse(window.localStorage.getItem("task-list")) || []
+  },
+
+  showTodos: () => {
+    document.querySelector("#todo-list-root").innerHTML = "";
+    const tasks = this.loadTodos();
+    tasks.forEach((value) => {
+      document.querySelector("#todo-list-root").innerHTML += `
+            <li class="todo-list__item">
+                <span>${value}</span>
+                <i class="fa fa-trash delete"></i>
+            </li>
+        `;
+    })
+
 }
-
-// function getCookie(key) {
-//     // var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
-//    // return keyValue ? keyValue[2] : null;
-//     return unescape(document.cookie);
-// }
-
-//remove cookie
-// function eraseCookie(key) {
-//     var keyValue = getCookie(key);
-//     setCookie(key, keyValue, '-1');
-// }
-
-function cookieElements() {
-  let elements = document.getElementById("todo-list-root").childNodes;
-  setCookie("task-list", elements);
-  let match = getCookie("task-list");
-  console.log(match);
-}
-
-//---------------OLD CODE----------------------//
-
-// //set cookies
-// function setCookie(cname, cvalue, exdays) {
-//     var d = new Date();
-//     d.setTime(d.getTime() + (exdays*24*60*60*30));
-//     var expires = "expires="+ d.toUTCString();
-//     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-// }
-
-// //get cookies
-// function getCookie(cname) {
-//     var name = cname + "=";
-//     var decodedCookie = decodeURIComponent(document.cookie);
-//     var ca = decodedCookie.split(';');
-//     for (var i = 0; i <ca.length; i++) {
-//         var c = ca[i];
-//         while (c.charAt(0) == ' ') {
-//             c = c.substring(1);
-//         }
-//         if (c.indexOf(name) == 0) {
-//             return c.substring(name.length, c.length);
-//         }
-//     }
-//     return ca;
-// }
-
-// //check cookies
-// function checkCookie() {
-//     var usercookie = getCookie("task-list");
-//     if (usercookie === "") {
-//         setCookie("task-list", "", 30);
-//     } else {
-//         document.getElementById("todo-list-root").innerHTML = usercookie;
-//     }
-// }
-
-// function cookieElements(){
-//     let elements = document.getElementById("todo-list-root").childNodes;
-//     setCookie("task-list", elements, 30);
-// }
+};
